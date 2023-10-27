93942f5227b158c1f9671dac15958f2cfab2db73
BBIYAKYEE7 <dadalgojr@gmail.com>
2023-10-28 02:00:24 +0900

    Android

---
+++ Play_Us(and)/app/src/main/java/com/example/play_us/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.play_us
+
+import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.webkit.WebChromeClient
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val myWebView: WebView = findViewById(R.id.webView)
+
+        myWebView.settings.run {
+            javaScriptEnabled = true
+            javaScriptCanOpenWindowsAutomatically = true
+            setSupportMultipleWindows(true)
+        }
+
+        myWebView.webViewClient = WebViewClient()
+        myWebView.webChromeClient = WebChromeClient()
+        myWebView.loadUrl("https://play-us.netlify.app/")
+
+
+    }
+
+    override fun onBackPressed() {
+
+        val myWebView: WebView = findViewById(R.id.webView)
+        if(myWebView.canGoBack()){
+            myWebView.goBack()
+        }else{
+            super.onBackPressed()
+        }
+    }
+}
