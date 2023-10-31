7f3742684dd81b721d6b932d2baa74c60d0b989d
BBIYAKYEE7 <dadalgojr@gmail.com>
2023-10-31 23:34:53 +0900

    Codes

--- Play_Us(and)/app/src/main/java/com/example/play_us/MainActivity.kt
+++ Play_Us(and)/app/src/main/java/com/example/play_us/MainActivity.kt
@@ -1,29 +1,101 @@
 package com.example.play_us
 
-import androidx.appcompat.app.AppCompatActivity
 import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Bundle
-import android.webkit.WebView
-import android.webkit.WebViewClient
-import android.webkit.WebChromeClient
+import android.webkit.*
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var locationManager: LocationManager
+    private lateinit var myWebView: WebView
+
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val myWebView: WebView = findViewById(R.id.webView)
+        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
 
-        myWebView.settings.run {
+        myWebView = findViewById(R.id.webView)
+
+        myWebView.settings.apply {
             javaScriptEnabled = true
             javaScriptCanOpenWindowsAutomatically = true
             setSupportMultipleWindows(true)
+            domStorageEnabled = true // 추가: DOM 저장소를 활성화합니다.
         }
 
         myWebView.webViewClient = WebViewClient()
-        myWebView.webChromeClient = WebChromeClient()
+        myWebView.webChromeClient = object : WebChromeClient() {
+            override fun onGeolocationPermissionsShowPrompt(origin: String, callback: GeolocationPermissions.Callback) {
+                callback.invoke(origin, true, false)
+            }
+        }
+        myWebView.addJavascriptInterface(WebAppInterface(), "Android")
         myWebView.loadUrl("https://play-us.netlify.app/")
 
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                0
+            )
+        } else {
+            getLocation()
+        }
+    }
 
+    private fun getLocation() {
+        try {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0f, locationListener)
+        } catch (e: SecurityException) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                0
+            )
+        }
+    }
+
+    private val locationListener: LocationListener = object : LocationListener {
+        override fun onLocationChanged(location: Location) {
+            val latitude = location.latitude
+            val longitude = location.longitude
+            myWebView.evaluateJavascript("showLocationOnMap($latitude, $longitude);", null)
+        }
+
+        override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {}
+
+        override fun onProviderEnabled(provider: String) {}
+
+        override fun onProviderDisabled(provider: String) {}
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 0) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getLocation()
+            }
+        }
+    }
+
+    private inner class WebAppInterface {
+        @JavascriptInterface
+        fun showToast(toast: String) {
+            Toast.makeText(applicationContext, toast, Toast.LENGTH_SHORT).show()
+        }
     }
 }
